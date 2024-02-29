d1b7827d51b2240a6af65f76ec107cc623a072d1
Pramod Selvaraj <pramod279@yahoo.com>
2024-02-29 08:55:19 +0530

    Bounce Theme Changes

--- uikit/src/main/java/com/sendbird/uikit/activities/ChannelActivity.java
+++ uikit/src/main/java/com/sendbird/uikit/activities/ChannelActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -67,6 +68,8 @@ public class ChannelActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(getResources().getColor(R.color.primary_500, this.getTheme()));
         setTheme(SendbirdUIKit.getDefaultThemeMode().getResId());
         setContentView(R.layout.sb_activity);
 
